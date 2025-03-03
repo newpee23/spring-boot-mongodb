46d81d334c52e3006980f817339a19df1445ebf5
Peerapat Mueangmo <139107392+newpee23@users.noreply.github.com>
2025-03-03 16:49:57 +0700

    Update CommentsEntity.java

--- src/main/java/com/example/spring/App/model/Entity/CommentsEntity.java
+++ src/main/java/com/example/spring/App/model/Entity/CommentsEntity.java
@@ -1,6 +1,5 @@
 package com.example.spring.App.model.Entity;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
